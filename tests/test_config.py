62f3b7c8af555751ef43ddb9d3fe013f1a26fc3b
Devendra Prasad <devendra.p@hcl.com>
2022-08-14 09:02:05 +0000

    web structure added

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,5 +1,6 @@
 import pytest
 
+
 class NotInRange(Exception):
     def __init__(self, message="value not in range"):
         self.message = message
@@ -9,9 +10,9 @@ class NotInRange(Exception):
 def test_generic():
     a = 5
     with pytest.raises(NotInRange):
-       if a not in range(10,20):
-        raise NotInRange
-    
+        if a not in range(10, 20):
+            raise NotInRange
+
 
 def another_test():
     assert 1 == 2
